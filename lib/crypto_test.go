ee7993268c64a3bfbe2d6a2c4d2e44e320f93dca
Yuji Mise <miseyu2000@gmail.com>
2018-12-27 15:22:18 +0900

    add: cryto unit test

--- lib/crypto_test.go
+++ lib/crypto_test.go
@@ -3,5 +3,9 @@ package lib
 import "testing"
 
 func TestEncryptSha256(t *testing.T) {
-
+	actual := EncryptSha256("hogehoge")
+	expected := "4c716d4cf211c7b7d2f3233c941771ad0507ea5bacf93b492766aa41ae9f720d"
+	if actual != expected {
+		t.Errorf("got: %v\nwant: %v", actual, expected)
+	}
 }
